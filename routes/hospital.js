b9be48d332c4d3c950127cab5a659323bc783860
jonathanquehay <jjmore83@gmail.com>
2018-01-18 18:49:37 -0600

    Las operaciones completas

--- routes/hospital.js
+++ routes/hospital.js
@@ -2,13 +2,12 @@ var express = require('express');
 
 var app = express();
 
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
 var mdAutenticacion = require('../middleware/autenticacion');
 //importar el Schema
 
-var Usuario = require('../models/usuario');
+var Hospital = require('../models/hospital');
 
+//Obtener todos los hospitales
 //Definir rutas
 
 app.get('/', (req, res, next) => {
@@ -16,79 +15,73 @@ app.get('/', (req, res, next) => {
     var desde = req.query.desde || 0;
     desde = Number(desde);
 
-    Usuario.find({}, 'nombre email img role')
+    Hospital.find({})
         .skip(desde)
         .limit(5)
+        .populate('usuario', 'nombre email')
         .exec(
-            (err, usuarios) => {
+            (err, hospitales) => {
                 if (err) {
                     return res.status(500).json({
                         ok: false,
-                        mensaje: 'Error cargando usuarios!',
+                        mensaje: 'Error cargando hospital!',
                         errors: err
                     });
                 }
+                Hospital.count({}, (err, conteo) => {
 
-                Usuario.count({}, (err, conteo) => {
                     res.status(200).json({
                         ok: true,
-                        usuarios: usuarios,
+                        hospitales: hospitales,
                         total: conteo
                     });
-
                 });
-
             })
-
-
-
 });
 
 
 
 
 
-// ACTUALIZAR USUARIO
+// ACTUALIZAR HOSPITAL
 
 app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
     var id = req.params.id;
     var body = req.body;
 
-    Usuario.findById(id, (err, usuario) => {
+    Hospital.findById(id, (err, hospital) => {
 
         if (err) {
             return res.status(500).json({
                 ok: false,
-                mensaje: 'Error al buscar usuario!',
+                mensaje: 'Error al buscar hospital!',
                 errors: err
             });
         }
-        if (!usuario) {
+        if (!hospital) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'El usuario con el id ' + id + ' no existe',
-                errors: { message: 'No existe un usario con ese id' }
+                mensaje: 'El hospital con el id ' + id + ' no existe',
+                errors: { message: 'No existe un hospital con ese id' }
             });
         }
 
-        usuario.nombre = body.nombre;
-        usuario.email = body.email;
-        usuario.role = body.role;
+        hospital.nombre = body.nombre;
+        hospital.usuario = req.usuario._id;
 
-        usuario.save((err, usuarioGuardado) => {
+        hospital.save((err, hospitalGuardado) => {
             if (err) {
                 return res.status(400).json({
                     ok: false,
-                    mensaje: 'Error al actualizar usuario!',
+                    mensaje: 'Error al actualizar hospital!',
                     errors: err
                 });
             }
 
-            usuarioGuardado.password = ':)';
 
             res.status(200).json({
                 ok: true,
-                usuario: usuarioGuardado
+                hospital: hospitalGuardado
             });
 
         });
@@ -98,61 +91,57 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 });
 
 
-//CREAR UN NUEVO USUARIO
+//CREAR UN NUEVO HOSPITAL
 
 app.post('/', mdAutenticacion.verificaToken, (req, res) => {
     var body = req.body;
 
-    var usuario = new Usuario({
+    var hospital = new Hospital({
         nombre: body.nombre,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        img: body.img,
-        role: body.role
+        usuario: req.usuario._id
     });
 
     //guardar
 
-    usuario.save((err, usuarioGuardado) => {
+    hospital.save((err, hospitalGuardado) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'Error al crear usuario!',
+                mensaje: 'Error al crear hospital!',
                 errors: err
             });
         }
         res.status(201).json({
             ok: true,
-            usuario: usuarioGuardado,
-            usuariotoken: req.usuario
+            hospital: hospitalGuardado,
         });
     });
 });
 
-//ELIMINAR USUARIO POR ID
+//ELIMINAR HOSPITAL POR ID
 
 app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
     var id = req.params.id;
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Hospital.findByIdAndRemove(id, (err, hospitalBorrado) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'Error al borrar usuario!',
+                mensaje: 'Error al borrar hospital!',
                 errors: err
             });
         }
 
-        if (!usuarioBorrado) {
+        if (!hospitalBorrado) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'No existe un usuario con ese id!',
-                errors: { message: 'No existe un usuario con ese id' }
+                mensaje: 'No existe un hospital con ese id!',
+                errors: { message: 'No existe un hospital con ese id' }
             });
         }
 
         res.status(200).json({
             ok: true,
-            usuario: usuarioBorrado
+            hospital: hospitalBorrado
         });
     });
 });
---
+++ routes/upload.js
@@ -0,0 +1,164 @@
+var express = require('express');
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
+var app = express();
+
+var Usuario = require('../models/usuario');
+var Hospital = require('../models/hospital');
+var Medico = require('../models/medico');
+//Subir un archivo
+
+app.use(fileUpload());
+
+
+//Definir rutas. Next: Cuando se ejecute continue con las otras peticiones
+
+app.put('/:tipo/:id', (req, res, next) => {
+
+    var tipo = req.params.tipo;
+    var id = req.params.id;
+
+    //tipos de coleccion
+
+    var tiposValidos = ['hospitales', 'medicos', 'usuarios'];
+
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'Tipo de coleccion no es valida',
+            errors: { message: 'Tipo de coleccion no es valida' }
+        });
+    }
+
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'No selecciono nada',
+            errors: { message: 'Debe seleccionar una imagen' }
+        });
+    }
+
+    //Obtener nombre del archivo
+
+    var archivo = req.files.imagen;
+    var nombreCortado = archivo.name.split('.');
+    var extArchivo = nombreCortado[nombreCortado.length - 1];
+
+    //Solo validar las extensiones siguientes
+
+    var extValidas = ['png', 'jpg', 'gif', 'jpeg'];
+    if (extValidas.indexOf(extArchivo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'Extension no valida',
+            errors: { message: 'Las extensiones validas son' + extValidas.join(', ') }
+        });
+    }
+
+    //Nombre de archivo personalizado
+    var nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extArchivo}`;
+
+    //Mover el archivo del temporal a un path especifico
+    var path = `./uploads/${tipo}/${nombreArchivo}`;
+    archivo.mv(path, err => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al mover archivo',
+                errors: err
+            });
+
+        }
+
+        subirPorTipo(tipo, id, nombreArchivo, res)
+
+    });
+
+
+});
+
+function subirPorTipo(tipo, id, nombreArchivo, res) {
+    if (tipo === 'usuarios') {
+        Usuario.findById(id, (err, usuario) => {
+
+            if (!usuario) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Usuario no existe',
+                    errors: { message: 'Usuario no existe' }
+                });
+            }
+            var pathViejo = './uploads/usuarios/' + usuario.img;
+            //Si existe elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            usuario.img = nombreArchivo;
+            usuario.save((err, usuarioActualizado) => {
+
+                usuarioActualizado.password = ':)';
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de usuario actualizada',
+                    usuario: usuarioActualizado
+                });
+            });
+        });
+    }
+    if (tipo === 'medicos') {
+        Medico.findById(id, (err, medico) => {
+            if (!medico) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Medico no existe',
+                    errors: { message: 'Medico no existe' }
+                });
+            }
+            var pathViejo = './uploads/medicos/' + medico.img;
+            //Si existe elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            medico.img = nombreArchivo;
+            medico.save((err, medicoActualizado) => {
+
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de medico actualizada',
+                    medico: medicoActualizado
+                });
+            });
+        });
+
+    }
+    if (tipo === 'hospitales') {
+        Hospital.findById(id, (err, hospital) => {
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Hospital no existe',
+                    errors: { message: 'Hospital no existe' }
+                });
+            }
+            var pathViejo = './uploads/hospitales/' + hospital.img;
+            //Si existe elimina la imagen anterior
+            if (fs.existsSync(pathViejo)) {
+                fs.unlink(pathViejo);
+            }
+            hospital.img = nombreArchivo;
+            hospital.save((err, hospitalActualizado) => {
+
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Imagen de hospital actualizada',
+                    hospital: hospitalActualizado
+                });
+            });
+        });
+
+
+    }
+
+}
+
+module.exports = app;
